7b5a5eb0ce07aa16da0e8b3d73e98ca7c0ba6df9
Kumar-arc8434 <kumarxdkid@gmail.com>
2025-09-14 12:38:11 -0400

    Organize Tinkercad projects into folders

---
+++ fading_led1.ino
@@ -0,0 +1,20 @@
+// C++ code
+//'
+int ledPin = 9;
+int brightness = 0;
+int fade = 5;
+void setup()
+{
+  pinMode(ledPin, OUTPUT); 
+}
+
+void loop()
+{
+analogWrite(ledPin, brightness); 
+    brightness = brightness + fade; 
+  
+    if (brightness <= 0 || brightness >= 255) {
+    fade = -fade;
+  } 
+  delay(30); 
+}
